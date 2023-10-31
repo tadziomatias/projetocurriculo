0f0f8d9bc7a28b30f832a674b617c6ab1769beec
Igor Matias <tadziomatias@gmail.com>
2023-10-30 21:06:49 -0300

    Ajustes imagens "Joe"

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function toggleMode() {
   //substituir a imagem//
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./.vscode/assets/avatar_igor.png")
+    img.setAttribute("src", "assets/avatar_igor.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./.vscode/assets/avatar_igor.png")
+    img.setAttribute("src", "assets/avatar_igor.png")
   }
 }
